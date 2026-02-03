5a0e85bd89515f9fa1e6cbfe7ef057bf36765197
TeemotioBB <r66vtrhwkw@privaterelay.appleid.com>
2026-02-03 15:29:54 -0300

    Update sophia_bot_v7.2_clean.py

--- sophia_bot_v7.2_clean.py
+++ sophia_bot_v7.2_clean.py
@@ -56,7 +56,7 @@ GROK_KEY = "COLE_SUA_KEY_GROK_AQUI"
 
 # 📢 Links dos Canais (IMPORTANTE: Use os links públicos corretos)
 LINK_CANAL_PREVIAS = "https://t.me/previasdamayaofc"  # Seu canal de prévias
-LINK_CANAL_VIP = "https://t.me/+SEU_LINK_VIP_AQUI"     # Seu canal VIP (com +)
+LINK_CANAL_VIP = "https://t.me/Mayaoficial_bot"     # Seu canal VIP (com +)
 
 # 👤 Admin
 MEU_TELEGRAM_ID = "1293602874"  # Seu ID do Telegram
